1c96c5035ff737e17ea77ede213788af8dbf1043
yogendradevil <yogendrabijapur2002@gmail.com>
2023-11-22 15:55:34 +0530

    till 07useContext

---
+++ 07miniContext/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react'
+import UserContext from '../context/UserContext'
+
+
+function Profile() {
+  const {user} = useContext(UserContext);
+  
+    if(!user){
+        return (
+            <h1>Please Login</h1>
+        )
+    }
+
+  return (
+    <h1>Wellcome {user.username}</h1>
+  )
+}
+
+export default Profile
